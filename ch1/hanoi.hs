4341243394d32a967ad7810d31f5166f7730ce8d
Arnav <arnavsastry+github@gmail.com>
2013-08-09 23:54:14 -0500

    tried hanoi4

--- ch1/hanoi.hs
+++ ch1/hanoi.hs
@@ -1,3 +1,4 @@
+import Data.List
 type Peg = String
 type Move = (Peg, Peg)
 
@@ -5,3 +6,13 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a b c = [(a, b)]
 hanoi x a b c = (hanoi (x-1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (x-1) c b a)
+
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 1 a b _ _ = [(a, b)]
+hanoi4 n a b c d = minimumBy (\x y -> compare (length x) (length y)) $ map (\x -> hanoi4' n x a b c d) [1..((div n 2) + 1)]
+
+hanoi4' :: Integer -> Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4' 0 _ _ _ _ _ = []
+hanoi4' 1 _ a b _ _ = [(a, b)]
+hanoi4' n k a b c d = (hanoi (k) a c d) ++ (hanoi (n-k) a b d) ++ (hanoi (k) c b a)
